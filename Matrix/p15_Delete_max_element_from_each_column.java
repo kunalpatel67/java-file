0fbf94c1429f17c90a1cbdf8a6a8dcfaffa683e1
kunal patel <kunalpatel6705@gmail.com>
2025-06-27 11:57:49 +0530

    Update p15_Delete_max_element_from_each_column.java

--- Matrix/p15_Delete_max_element_from_each_column.java
+++ Matrix/p15_Delete_max_element_from_each_column.java
@@ -2,6 +2,31 @@ package Matrix;
 
 public class p15_Delete_max_element_from_each_column {
     public static void main(String[] args) {
+        int[][] mat = {
+                {10,2,3},
+                {40,50,6},
+                {70,8,90},
+                {100,11,12}};
 
+        int row = mat.length , col = mat[0].length;
+
+        for (int i = 0; i < col; i++){
+            int maxindex = 0;
+            for (int j = 0; j < row; j++){
+                if (mat[j][i] > mat[maxindex][i]){
+                    maxindex = j;
+                }
+            }
+            for (int j = maxindex; j < row - 1; j++){
+                mat[j][i] = mat[j+1][i];
+            }
+        }
+        row--;
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                System.out.print(mat[i][j] +" ");
+            }
+            System.out.println();
+        }
     }
 }
