00b3f1655ebf384b8c34397115247f50f6023a5f
kunal patel <kunalpatel6705@gmail.com>
2025-06-27 11:54:05 +0530

    Update p9_print_matrix_column_wise_in_zigzag_order.java

--- Matrix/p9_print_matrix_column_wise_in_zigzag_order.java
+++ Matrix/p9_print_matrix_column_wise_in_zigzag_order.java
@@ -3,22 +3,20 @@ package Matrix;
 public class p9_print_matrix_column_wise_in_zigzag_order {
     public static void main(String[] args) {
         int[][] mat = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9},
-                {10,11,12}};
+                {1, 2, 3},
+                {5, 6, 7},
+                {9, 10, 11},
+                {13, 14, 15}};
+        int row = mat.length;
+        int col = mat[0].length;
 
-        int row = mat.length , col = mat[0].length;
-
-        for (int i = 0; i < col; i++){
-            if (i % 2 == 0){
-                for (int j = 0; j < row; j++){
-                    System.out.print(mat[j][i] + " ");
-                }
-            }
-            else {
-                for (int j = row-1; j >= 0; j--){
-                    System.out.print(mat[j][i] + " ");
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++) {
+                if (j % 2 == 0) {
+                    System.out.print(mat[i][j]+" ");
+                } else {
+                    System.out.print(mat[row - i - 1][j]+" ");
                 }
             }
             System.out.println();
